07444f6fd235d4cf588b93934597e5b44841956a
MoonBeeJPG <agustinasoledad2608@gmail.com>
2022-09-17 17:48:58 +0000

    All task 3 files

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -12,12 +12,11 @@ window.addEventListener('DOMContentLoaded', (event) => {
   }
   $('.amenities > h4').text(array.join(', '));
   });
-});
-$getJSON('http://0.0.0.0:5001/api/v1/status/', function (data) {
-  if (data.status === 'OK') {
-     $('#api_status').addClass('available')
+  $.getJSON('http://0.0.0.0:5001/api/v1/status', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available')
     } else {
-     $('#api_status').removeClass('available')
+      $('#api_status').removeClass('available')
     }
-  };
+  })
 });
