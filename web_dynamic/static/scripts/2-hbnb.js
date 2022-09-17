4d734bce387040c1d9fb547ed6c0099b49ae3ed5
NikShiskobcki <nicolleshiskobcki@gmail.com>
2022-09-17 19:31:06 +0000

    fixes

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,22 +1,21 @@
-#!/usr/bin/node
 window.addEventListener('DOMContentLoaded', (event) => {
   let array = [];
   $('input:checkbox').change(function() {
-  if ($(this).is(":checked")) {
-    const x = ($(this).attr('data-name'));
-    array.push(x);
-  } else {
-    const n = ($(this).attr('data-name'));
-    const indx = array.indexOf(n);
-    array.splice(indx, 1);
-  }
-  $('.amenities > h4').text(array.join(', '));
-  });
-  $.getJSON('http://0.0.0.0:5001/api/v1/status', function (data) {
-    if (data.status === 'OK') {
-      $('#api_status').addClass('available')
+    if ($(this).is(":checked")) {
+      const n = ($(this).attr('data-name'));
+      array.push(n);
     } else {
-      $('#api_status').removeClass('available')
+      const n = ($(this).attr('data-name'));
+      const indx = array.indexOf(n);
+      array.splice(indx, 1);
     }
-  })
-});
+    $('.amenities > h4').text(array.join(', '));
+  });
+ });
+ $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
+    if (data.status == 200) {
+      $('div#api_status').addClass('available');
+    } else {
+      $('div#api_status').removeClass('available');
+    }
+  });
